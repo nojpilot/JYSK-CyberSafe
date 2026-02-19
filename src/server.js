2087ddb1e7d6673a18d1b10652e1e12284478d4d
xazimov <xazimov@fi.muni.cz>
2026-02-19 18:03:53 +0100

    Set trust proxy setting in Express app for improved security

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
+app.set('trust proxy', 1);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
